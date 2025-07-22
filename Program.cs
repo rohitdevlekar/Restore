1bf88571afc8462195a323d379a89afa88fd54dd
RohitDevlekarMSI <RohitDevlekarMSI@MSI>
2025-07-22 16:49:56 +0530

    Section 6 - Error Handling - Adding Exception Handling middleware part 1

--- Program.cs
+++ Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
 // if (app.Environment.IsDevelopment())
 // {
 //     app.MapOpenApi();
--- obj/Debug/net9.0/API.AssemblyInfo.cs
+++ obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b320439b2c574ae966a47c215a81ebcd9bc3077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fbca346cdb729305ee7299725ce99d291cc517")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
