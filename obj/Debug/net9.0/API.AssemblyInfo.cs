6119fd6757bd514c5a40fcef0c68e0c19ec9ecd4
RohitDevlekarMSI <RohitDevlekarMSI@MSI>
2025-07-26 18:25:21 +0530

    Section 6 - Error Handling - Debugging .Net Code

--- obj/Debug/net9.0/API.AssemblyInfo.cs
+++ obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf88571afc8462195a323d379a89afa88fd54dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49530af73470af3de13db18ddeffe5629a9689f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
