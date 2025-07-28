a1ce7ebb88056eb8bfca6d098dc5d944cb5a6426
RohitDevlekarMSI <RohitDevlekarMSI@MSI>
2025-07-28 10:42:22 +0530

    Section 7 - Adding the shopping cart feature - Adding the basket enritty

---
+++ Entities/Basket.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace API.Entities;
+
+public class Basket
+{
+    public int Id { get; set; }
+
+    public required string BasketId { get; set; }
+
+    public List<BasketItem> Items { get; set; } =  [];   
+
+}
