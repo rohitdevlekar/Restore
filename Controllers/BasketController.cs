6b6386241e25f2fb7381960de6845094f35951b5
RohitDevlekarMSI <RohitDevlekarMSI@MSI>
2025-07-28 16:33:21 +0530

    Section 7 - Adding the shopping cart feature - Adding the add item logic to controller

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -11,8 +11,7 @@ public class BasketController(StoreContext context) : BaseApiController
     [HttpGet]
     public async Task<ActionResult<Basket>> GetBasket()
     {
-        var basket = await context.Baskets.Include(x => x.Items).ThenInclude(x => x.Product)
-        .FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+        var basket = await RetriveBasket();
 
         if (basket == null) return NoContent();
 
@@ -25,16 +24,52 @@ public class BasketController(StoreContext context) : BaseApiController
     public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
     {
         // get basket
+        var basket = await RetriveBasket();
+
         // create basket
+        basket ??= createBasket();
+
         // get product
+        var product = await context.Products.FindAsync(productId);
+
+        if (product == null) return BadRequest("Problem adding item to basket");
+
         // add item to basket
+        basket.AddItem(product , quantity);
+
+
         // save changes
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (result) return CreatedAtAction(nameof(GetBasket) , basket);
 
 
-        return StatusCode(201);
+        return BadRequest("Problem updating basket");
     }
 
+    private Basket createBasket()
+    {
+        var basketId = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions
+        {
+            IsEssential = true,
+            Expires = DateTime.UtcNow.AddDays(30)
+        };
 
+        Response.Cookies.Append("basketId", basketId, cookieOptions);
+        var basket = new Basket { BasketId = basketId };
+
+        context.Baskets.Add(basket);
+
+        return basket;
+
+    }
+
+    private async Task<Basket?> RetriveBasket()
+    {
+        return await context.Baskets.Include(x => x.Items).ThenInclude(x => x.Product).FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+
+    }
 
     [HttpDelete]
     public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
