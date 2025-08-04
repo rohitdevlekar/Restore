41628d015abc1d4c4ee0440ed382e953d1a42e71
RohitDevlekarMSI <RohitDevlekarMSI@MSI>
2025-08-04 16:49:47 +0530

    Section 8: Adding Sorting,Filtering,Searching and Pagination functionality - Adding pagination to API Part 2

--- RequestHelper/PaginationParams.cs
+++ RequestHelper/PaginationParams.cs
@@ -8,7 +8,7 @@ public class PaginationParams
 
     public int PageNumber { get; set; } = 1;
 
-    private int _pageSize;
+    private int _pageSize = 8;
     public int PageSize
     {
         get => _pageSize;
