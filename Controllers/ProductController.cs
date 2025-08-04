c01b231af1957b8ccdb23af728f2349e00229ae9
RohitDevlekarMSI <RohitDevlekarMSI@MSI>
2025-08-04 16:57:54 +0530

    Section 8: Adding Sorting,Filtering,Searching and Pagination functionality - Adding an endpoint to get the filters from the API

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,25 +10,43 @@ namespace API.Controllers
     public class ProductController(StoreContext context) : BaseApiController
     {
         [HttpGet]
-        public async Task< ActionResult <List<Product>>> GetProducts( [FromQuery] ProductParams productParams)
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
-            var query  = context.Products
+            var query = context.Products
             .Sort(productParams.OrderBy)
             .Search(productParams.SearchTerm)
-            .Filter(productParams.Brands , productParams.Types)
+            .Filter(productParams.Brands, productParams.Types)
             .AsQueryable();
 
-            return await context.Products.ToListAsync();
+            var products = await PagedList<Product>.ToPagesList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.Metadata);
+
+            return Ok(new { Items = products, products.Metadata });
         }
 
         [HttpGet("{id}")]
-        public async Task< ActionResult <Product>> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await  context.Products.FindAsync(id);
+            var product = await context.Products.FindAsync(id);
 
             if (product == null) return NotFound();
 
             return product;
         }
+
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await context.Products.Select(x => x.Brand).Distinct().ToListAsync();
+            var types = await context.Products.Select(x => x.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
+        }
+         
+
+
+
     }
 }
